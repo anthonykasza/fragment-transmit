70994cda0b5423b362013eca14fb498759683762
ak <a@k>
2022-11-20 10:58:11 -0500

    fix range

--- examples/range.go
+++ examples/range.go
@@ -1,24 +1,24 @@
 package main
 
-import ("fmt")
+import ("fmt"; "reflect")
 
 
 func main() {
 
   // range'ing a string results in int32's, this is surprising
   s := "hello123"
-  fmt.Println(fmt.Printf("%T", s))
+  fmt.Println(reflect.TypeOf(s))
   for _,ord := range s {
-    fmt.Println(fmt.Printf("%T", ord), ord, string(ord))
+    fmt.Println(reflect.TypeOf(ord), ord, string(ord))
   }
 
   fmt.Println("\n======\n")
 
   // range'ing an arrary of string results in strings, as expected
   a := []string{"hello", "123"}
-  fmt.Println(fmt.Printf(a))
+  fmt.Println(a)
   for _,elem := range a {
-    fmt.Println(fmt.Printf(elem), elem)
+    fmt.Println(elem)
   }
 
   // it's funny to me that go cannot infer the type of the following string array
